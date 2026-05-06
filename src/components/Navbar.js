f02d977db13bd26319e4cebc3eceeb9db522cab3
Aakash Patil <akashnp25274@gmail.com>
2026-05-06 12:22:16 +0530

    testing cicd

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,7 +20,7 @@ const Navbar = () => {
     <nav className="navbar">
       <div className="container flex items-center justify-between">
         <Link to="/" className="flex items-center gap-2" style={{ fontSize: '1.25rem', fontWeight: 700 }}>
-          <Package className="text-primary" /> Aakash Patil Commerce
+          <Package className="text-primary" /> THIS IS TESTING CICD
         </Link>
         <div className="nav-links">
           {user?.role === 'admin' && (
